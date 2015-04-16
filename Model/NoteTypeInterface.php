e8798e1fd8391c2214426826888a0f424be75a9b
Christopher West <9ninemind@gmail.com>
2015-04-16 15:51:57 -0600

    adds note type

---
+++ Model/NoteTypeInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace BlackBoxCode\Pando\Bundle\TicketBundle\Model;
+
+use BlackBoxCode\Pando\Bundle\NoteBundle\Model\NoteTypeInterface as NoteBundleNoteTypeInterface;
+
+interface NoteTypeInterface extends NoteBundleNoteTypeInterface
+{
+    const NOTE = 'Note';
+}
